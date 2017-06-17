6396b981cc78ad5076991ae2b6d9f6b537d59abe
dognie <dognie7@gmail.com>
2017-06-17 21:24:40 +0800

    delete leaflet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import L from 'leaflet';
 import './App.css';
-const position = [51.0, -0.09];
-const L.TileLayer = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?';
-const L.attribution = 'pk.eyJ1IjoiZG9nbmllIiwiYSI6ImNqM3ppbDQ2NDA0Nmwyd3BjM2tqeWRxbnEifQ.G3A-2ZMQsIS1qwOQv8Gi8A';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <Map
-          style={{height: "100vh"}}
-          center={position}
-          zoom={10}>
-          <TileLayer
-            url="<TileLayer>"
-            attribution="<L.attribution>" />
-        </Map>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
